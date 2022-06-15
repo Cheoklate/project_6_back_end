c684d4317214f1513eb77058d3a14ba5099f017a
smdewi <smdewi2020@gmail.com>
2022-06-15 12:46:23 +0800

    update Schema, nested structure, draft1

--- models/User.ts
+++ models/User.ts
@@ -4,22 +4,67 @@ const friendSchema: Schema = new Schema({
 	userName: String,
 });
 
+const friendRequestSentSchema: Schema = new Schema({
+  userName: String,
+});
+
+const friendRequestReceivedSchema: Schema = new Schema({
+  userName: String,
+});
+
+const sharedWithSchema: Schema = new Schema({
+  userName: String,
+});
+
+const habitActionSchema: Schema = new Schema({
+  action: String,
+	date: Date,
+});
+
+const habitStreakSchema: Schema = new Schema({
+	totalCompleted: Number,
+	completedCount: Number,
+	streakCount: Number,
+	numberSkips: Number,
+});
+
+const reminderSchema: Schema = new Schema({
+	reminderMethod: String,
+	reminderMethodContact: String,
+	reminderTime: Date,
+});
+
+const habitSchema: Schema = new Schema({
+	habitName: String,
+	habitDesc: String,
+	isPublic: Boolean,
+	frequencyUnit: String,
+	frequencyNumber: Number,
+	sharedWith: [sharedWithSchema],
+	habitAction: [habitActionSchema],
+	habitStreak: [habitStreakSchema],
+	reminders: [reminderSchema],
+});
+
 const userSchema: Schema = new Schema(
-	{
-		firstName: String,
-		lastName: String,
-		userName: String,
-		email: {
-			type: String,
-			lowercase: true,
-			required: true,
-			unique: true,
-			trim: true,
-			match: /.+\@.+\..+/,
-		},
-		password: { type: String, required: true },
-		userFriends: [friendSchema],
-	},
-	{ timestamps: true }
+  {
+    firstName: String,
+    lastName: String,
+    userName: String,
+    email: {
+      type: String,
+      lowercase: true,
+      required: true,
+      unique: true,
+      trim: true,
+      match: /.+\@.+\..+/,
+    },
+    password: { type: String, required: true },
+    userFriends: [friendSchema],
+    friendRequestSent: [friendRequestSentSchema],
+    friendRequestReceived: [friendRequestReceivedSchema],
+		habits: [habitSchema],
+  },
+  { timestamps: true }
 );
 export default model('User', userSchema);
