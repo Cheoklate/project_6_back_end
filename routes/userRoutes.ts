f2171d7045b31f5c6bf6c428b6384c2453c777b4
gracethedragon <gracethedragon@hotmail.com>
2022-06-21 11:47:42 +0800

    get all friends

--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 import UserController from '../controllers/userController';
 import HabitController from '../controllers/habitController';
+import FriendController from '../controllers/friendController'
 const router: Router = Router();
 
 router.post('/login', UserController.login);
 router.post('/signup', UserController.signup);
 router.post('/createhabit', HabitController.createHabit);
-router.get('/allhabits', HabitController.allHabits);router.post('/updatehabit', HabitController.updateHabit);
+router.get('/allhabits', HabitController.allHabits);
+router.post('/updatehabit', HabitController.updateHabit);
+router.get('/friends', FriendController.allFriends)
 
 export default router;
